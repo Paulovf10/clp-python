e979be8f934b5d0c1beaddc6527749880dc8fca8
Paulovf10 <p.victorsousapro@gmail.com>
2022-09-21 16:30:18 -0300

    Final

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# clp-python
+
+
+<img src="https://ccomp.ufsj.edu.br/images/logo/logo_white.png" width="300" height="100" align="left"/>
+
+<br>
+<br>
+<br>
+
+#
+# Trabalho prático 1 - Primeira parte
+
+Disciplina: Conceitos de linguagem de programação
+
+Docente: Dárlinton Barbosa Feres Carvalho
+
+Aluno: Paulo Victor Fernandes Sousa 
+
+Número de matrícula: 192050108
+
+# Sobre o projeto
+Neste projeto foi implementado um sistema de vendas com o objetivo de mostrar a parte prática da linguagem selecionada pelo grupo.
+O projeto tem um menu interativo que permite ao usuário navegar por todas as entidades do programa.
+
+São elas Cliente uma classe que contém uma super classe chamada pessoa, Produto e Vendas, as duas últimas contém uma superclasse chamada Totalizável que contém métodos usados nessas classes.
+
+Ao longo da implementação foi usado try except para evitar danos causados pelo usuário.
+
+As classes foram criadas no mesmo script para poder mostrar o poder do python de aceitar vários tipos de programação.
+
+# Execução
+Para executar o projeto basta digitar o comando:
+
+- python main.py
+
+
--- pessoa.py
+++ pessoa.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Pessoa(object):
     def __init__(self, name, address):
         self.nome = name
@@ -65,9 +68,9 @@ class ItemVenda(Totalizavel):
 
 
 class Venda(Totalizavel):
-    def __init__(self, number, date, client: Cliente, itens: []):
+    def __init__(self, number, data_nasc, client: Cliente, itens: []):
         self.numero = number
-        self.data = date
+        self.data = data_nasc
         self.cliente = client
         self.itens = itens
         total = 0
@@ -84,190 +87,218 @@ class Venda(Totalizavel):
 
 print("Bem vindos !!!")
 
-Pessoas = []
 Clientes = []
 Produtos = []
 Vendas = []
 while True:
     print('------------------Menu inicial------------------')
-    print("1-Pessoa \n2-Cliente \n3-Produto \n4-Venda")
+    print("1-Cliente \n2-Produto \n3-Venda")
     cod = int(input("Qual entidade você quer manipular:"))
+
     if cod == 1:
-        print('------------------Menu Pessoas------------------')
-        print("1-Cadastrar uma pessoa \n2-Visualizar uma pessoa \n3-Alterar uma pessoa \n4-Excluir uma pessoa")
-        cod = int(input("O que deseja fazer: "))
-        if cod == 1:
-            nome = str(input('Digite o nome da pessoa: '))
-            endereco = str(input('Digite o endereco da pessoa: '))
-            pessoa = Pessoa(nome, endereco)
-            Pessoas.append(pessoa)
+        status = 1
+        while status != 0:
+            print('------------------Menu Clientes------------------')
+            print(
+                "1-Cadastrar um cliente \n2-Visualizar um cliente \n3-Alterar um cliente \n"
+                "4-Excluir um cliente \n0-Voltar ao menu inicial\n")
+            status = int(input("O que deseja fazer: "))
+            if status == 1:
+                nome = str(input('Digite o nome do cliente: '))
+                endereco = str(input('Digite o endereço do cliente: '))
+                rg = str(input('Digite o rg do cliente: '))
+                data_nascimento = str(input('Digite a data de nascimento do cliente: '))
+                try:
+                    date = datetime.strptime(data_nascimento, '%d/%m/%Y').date()
+                except:
+                    print(
+                        "\n-Fomatação de data invalida!!! Cadastro cancelado\nA data deve estar no formato %d/%m/%Y\n")
+                    continue
+                cliente = Cliente(nome, endereco, rg, date)
+                Clientes.append(cliente)
 
-        elif cod == 2:
-            for elem in Pessoas:
-                if elem.nome:
-                    print(elem.nome)
+            elif status == 2:
+                print("\n-Clientes cadastrados")
+                for elem in Clientes:
+                    if elem.nome:
+                        print('---------------------')
+                        print("Nome:", elem.nome)
+                        print("Rg:", elem.rg)
+                        print("Endereço:", elem.enderece)
+                        print("Data de nascimento:", elem.data_nascimento)
 
-        elif cod == 3:
-            ind = str(input("Digite o nome da pessoa que desejsa alterar: "))
-            flag = 0
-            for elem in Pessoas:
-                if elem.nome == ind:
-                    nome = input('Digite o novo nome: ')
-                    enderece = input('Digite o novo enderece: ')
-                    elem.alterar(nome, enderece)
-                    flag = 1
-            if flag == 0:
-                print("Não existe ninguem cadastrado com esse nome")
+            elif status == 3:
+                ind = str(input("Digite o nome do cliente que desejsa alterar: "))
+                flag = 0
+                for elem in Clientes:
+                    if elem.nome == ind:
+                        nome = input('Digite o novo nome: ')
+                        endereco = input('Digite o novo enderece: ')
+                        rg = input('Digite o novo rg: ')
+                        data_nascimento = input('Digite a nova data de nascimento: ')
+                        try:
+                            date = datetime.strptime(data_nascimento, '%d/%m/%Y').date()
+                        except:
+                            print(
+                                "\n-Fomatação de data invalida!!! Cadastro cancelado\n"
+                                "A data deve estar no formato %d/%m/%Y\n")
+                            continue
+                        elem.alterar_cli(rg, data_nascimento, nome, endereco)
+                        flag = 1
+                if flag == 0:
+                    print("Não existe ninguem cadastrado com esse nome")
 
-        elif cod == 4:
-            ind = str(input("Digite o nome da pessoa que deseja excluir: "))
-            flag = 0
-            for elem in Pessoas:
-                if elem.nome == ind:
-                    elem.remover()
-                    flag = 1
-            if flag == 0:
-                print("Não existe ninguem cadastrado com esse nome")
-        else:
-            print("Codigo invalido")
-
-    #  Cliente
-    elif cod == 2:
-        print('------------------Menu Clientes------------------')
-        print("1-Cadastrar um cliente \n2-Visualizar um cliente \n3-Alterar um cliente \n4-Excluir um cliente")
-        cod = int(input("O que deseja fazer: "))
-        if cod == 1:
-            nome = str(input('Digite o nome do cliente: '))
-            endereco = str(input('Digite o endereço do cliente: '))
-            rg = str(input('Digite o rg do cliente: '))
-            data_nascimento = str(input('Digite a data de nascimento do cliente: '))
-            cliente = Cliente(nome, endereco, rg, data_nascimento)
-            Clientes.append(cliente)
-
-        elif cod == 2:
-            for elem in Clientes:
-                if elem.nome:
-                    print(elem.nome)
-
-        elif cod == 3:
-            ind = str(input("Digite o nome do cliente que desejsa alterar: "))
-            flag = 0
-            for elem in Clientes:
-                if elem.nome == ind:
-                    nome = input('Digite o novo nome: ')
-                    endereco = input('Digite o novo enderece: ')
-                    rg = input('Digite o novo enderece: ')
-                    data_nascimento = input('Digite o novo enderece: ')
-                    elem.alterar_cli(rg, data_nascimento, nome, endereco)
-                    flag = 1
-            if flag == 0:
-                print("Não existe ninguem cadastrado com esse nome")
-
-        elif cod == 4:
-            ind = str(input("Digite o nome do cliente que deseja excluir: "))
-            flag = 0
-            for elem in Clientes:
-                if elem.nome == ind:
-                    elem.remover_cli()
-                    flag = 1
-            if flag == 0:
-                print("Não existe ninguem cadastrado com esse nome")
-        else:
-            print("Codigo invalido")
+            elif status == 4:
+                ind = str(input("Digite o nome do cliente que deseja excluir: "))
+                flag = 0
+                for elem in Clientes:
+                    if elem.nome == ind:
+                        elem.remover_cli()
+                        flag = 1
+                if flag == 0:
+                    print("Não existe ninguem cadastrado com esse nome")
+            else:
+                print("Codigo invalido")
 
     #  Produto
-    elif cod == 3:
-        print('------------------Menu Produtos------------------')
-        print("1-Cadastrar um produto \n2-Visualizar um produto \n3-Alterar um produto \n4-Excluir um produto")
-        cod = int(input("O que deseja fazer: "))
-        if cod == 1:
-            codigo = int(input('Digite o codigo do produto: '))
-            nome = str(input('Digite o nome do produto: '))
-            valor = float(input('Digite o valor do produto: '))
-            produto = Produto(codigo, nome, valor)
-            Produtos.append(produto)
+    elif cod == 2:
+        status = 1
+        while status != 0:
+            print('------------------Menu Produtos------------------')
+            print("1-Cadastrar um produto \n2-Visualizar um produto \n3-Alterar um produto \n"
+                  "4-Excluir um produto \n0-Voltar ao menu inicial\n")
+            status = int(input("O que deseja fazer: "))
+            if status == 1:
+                try:
+                    codigo = int(input('Digite o codigo do produto: '))
+                except ValueError:
+                    print(
+                        "\n-Entrada invalida!!! Cadastro cancelado\nO codigo deve ser um número inteiro\n")
+                    continue
+                nome = str(input('Digite o nome do produto: '))
+                try:
+                    valor = float(input('Digite o valor do produto: '))
+                except ValueError:
+                    print(
+                        "\n-Entrada invalida!!! Cadastro cancelado\nO valor deve ser um número\n")
+                    continue
+                produto = Produto(codigo, nome, valor)
+                Produtos.append(produto)
 
-        elif cod == 2:
-            for elem in Produtos:
-                if elem.nome:
-                    print(elem.nome)
+            elif status == 2:
+                print("\n-Produtos cadastrados")
+                for elem in Produtos:
+                    if elem.nome:
+                        print('---------------------')
+                        print("Nome:", elem.nome)
+                        print("Código:", elem.codigo)
+                        print("Valor:", elem.valor)
 
-        elif cod == 3:
-            ind = str(input("Digite o nome do produto que deseja alterar: "))
-            flag = 0
-            for elem in Produtos:
-                if elem.nome == ind:
-                    codigo = input('Digite o novo codigo: ')
-                    nome = input('Digite o novo nome: ')
-                    valor = input('Digite o novo valor: ')
-                    elem.alterar_prod(codigo, nome, valor)
-                    flag = 1
-            if flag == 0:
-                print("Não existe nenhum produto cadastrado com esse nome")
+            elif status == 3:
+                ind = str(input("Digite o nome do produto que deseja alterar: "))
+                flag = 0
+                for elem in Produtos:
+                    if elem.nome == ind:
+                        try:
+                            codigo = int(input('Digite o novo codigo: '))
+                        except ValueError:
+                            print(
+                                "\n-Entrada invalida!!! Cadastro cancelado\nO codigo deve ser um número inteiro\n")
+                            continue
+                        nome = input('Digite o novo nome: ')
+                        try:
+                            valor = float(input('Digite o novo valor: '))
+                        except ValueError:
+                            print(
+                                "\n-Entrada invalida!!! Cadastro cancelado\nO valor deve ser um número \n")
+                            continue
+                        elem.alterar_prod(codigo, nome, valor)
+                        flag = 1
+                if flag == 0:
+                    print("Não existe nenhum produto cadastrado com esse nome")
 
-        elif cod == 4:
-            ind = str(input("Digite o nome do produto que deseja excluir: "))
-            flag = 0
-            for elem in Produtos:
-                if elem.nome == ind:
-                    elem.remover_prod()
-                    flag = 1
-            if flag == 0:
-                print("Não existe nenhum produto cadastrado com esse nome")
-        else:
-            print("Codigo invalido")
+            elif status == 4:
+                ind = str(input("Digite o nome do produto que deseja excluir: "))
+                flag = 0
+                for elem in Produtos:
+                    if elem.nome == ind:
+                        elem.remover_prod()
+                        flag = 1
+                if flag == 0:
+                    print("Não existe nenhum produto cadastrado com esse nome")
+            else:
+                print("Codigo invalido")
 
     # Venda
-    elif cod == 4:
-        print('------------------Menu Vendas------------------')
-        ItemVendas = []
-        print("1-Cadastrar uma venda \n2-Visualizar uma venda\n3-Alterar umavenda \n4-Excluir uma venda")
-        cod = int(input("O que deseja fazer: "))
-        if cod == 1:
-            numero = int(input('Digite o número da sua venda: '))
-            data = str(input('Digite a data da sua venda: '))
-            cliente = str(input('Digite o nome do seu cliente: '))
-            cod2 = 1
-            while cod2 != 0:
-                print('------------------Menu de itens------------------')
-                print("1-Adicionar item \n2-Visualizar itens adicionados \n4-Remover item \n0-Finalisar venda")
-                cod2 = int(input('O que deseja fazer: '))
-                if cod2 == 1:
-                    produto = str(input('Digite o nome do produto: '))
-                    quantidade = int(input('Digite a quantidade: '))
-                    for elem in Produtos:
-                        if elem.nome == produto:
-                            Item = ItemVenda(elem, quantidade)
-                            ItemVendas.append(Item)
-                elif cod2 == 2:
-                    for elem in ItemVendas:
-                        print(elem.produto.nome)
-                        print(elem.quantidade)
-                        print(elem.valor)
-            for elem in Clientes:
-                if elem.nome == cliente:
-                    venda = Venda(numero, data, elem, ItemVendas)
-                    Vendas.append(venda)
+    elif cod == 3:
+        status = 1
+        while status != 0:
+            print('------------------Menu Vendas------------------')
+            ItemVendas = []
+            print("1-Cadastrar uma venda \n2-Visualizar uma venda \n3-Excluir uma venda \n0-Voltar ao menu inicial\n")
+            status = int(input("O que deseja fazer: "))
+            if status == 1:
+                try:
+                    numero = int(input('Digite o número da sua venda: '))
+                except ValueError:
+                    print(
+                        "\n-Entrada invalida!!! Cadastro cancelado\nO número deve ser um número inteiro\n")
+                    continue
+                data = str(input('Digite a data da sua venda: '))
+                try:
+                    date = datetime.strptime(data, '%d/%m/%Y').date()
+                except:
+                    print("\n-Fomatação de data invalida!!! Cadastro cancelado\n"
+                          "A data deve estar no formato %d/%m/%Y\n")
+                    continue
+                cliente = str(input('Digite o nome do seu cliente: '))
+                cod2 = 1
+                while cod2 != 0:
+                    print('------------------Menu de itens------------------')
+                    print("1-Adicionar item \n2-Visualizar itens adicionados \n4-Remover item \n0-Finalisar venda ")
+                    cod2 = int(input('O que deseja fazer: '))
+                    if cod2 == 1:
+                        produto = str(input('Digite o nome do produto: '))
+                        try:
+                            quantidade = int(input('Digite a quantidade: '))
+                        except ValueError:
+                            print(
+                                "\n-Entrada invalida!!! Cadastro cancelado\nA quantidade deve ser um número inteiro\n")
+                            continue
+                        for elem in Produtos:
+                            if elem.nome == produto:
+                                Item = ItemVenda(elem, quantidade)
+                                ItemVendas.append(Item)
+                    elif cod2 == 2:
+                        for elem in ItemVendas:
+                            print("Nome:", elem.produto.nome)
+                            print("Quantidade:", elem.quantidade)
+                            print("Valor:", elem.valor)
+                for elem in Clientes:
+                    if elem.nome == cliente:
+                        venda = Venda(numero, data, elem, ItemVendas)
+                        Vendas.append(venda)
 
-        elif cod == 2:
-            for elem in Vendas:
-                print('---------------------')
-                print(elem.numero)
-                print(elem.data)
-                print(elem.cliente.nome)
-                print(elem.total)
+            elif status == 2:
+                print("\n-Vendas cadastradas")
+                for elem in Vendas:
+                    print('---------------------')
+                    print(elem.numero)
+                    print(elem.data)
+                    print(elem.cliente.nome)
+                    print(elem.total)
 
-        elif cod == 3:
-            ind = str(input("Digite o número da venda que deseja excluir: "))
-            flag = 0
-            for elem in Vendas:
-                if elem.numero == ind:
-                    elem.remover_venda()
-                    flag = 1
-            if flag == 0:
-                print("Não existe nenhum produto cadastrado com esse nome")
-        else:
-            print("Codigo invalido")
+            elif status == 3:
+                ind = str(input("Digite o número da venda que deseja excluir: "))
+                flag = 0
+                for elem in Vendas:
+                    if elem.numero == ind:
+                        elem.remover_venda()
+                        flag = 1
+                if flag == 0:
+                    print("Não existe nenhum produto cadastrado com esse nome")
+            else:
+                print("Codigo invalido")
 
     elif cod == 0:
         break
